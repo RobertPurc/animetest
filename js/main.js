17ac5c32501c6dbcb73f5c54382e8f7ed6c82b98
RobertPurc <robert.purc12@gmail.com>
2021-02-14 18:50:10 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// alert("wolololo");
+
+const showmenu = document
+  .querySelector(".menu__btn")
+  .addEventListener("click", () => {
+    const nav = document.querySelector(".navigation__nav");
+    nav.classList.toggle("show_menu");
+
+    const btn = document.querySelector(".menu__btn");
+    btn.classList.toggle("burger");
+  });
+
+const showLogin = document
+  .getElementById("login")
+  .addEventListener("click", () => {
+    const form = document.querySelector(".login");
+
+    const screenSize = window.matchMedia("(max-width: 800px)");
+
+    function closeLogin(screen) {
+      if (screen.matches) {
+        const logInBtn = document.querySelector(".login__span");
+        logInBtn.classList.toggle("login--close");
+      } else {
+        console.log("kurwa");
+      }
+    }
+
+    const screen = window.matchMedia("(min-width: 800px)");
+    closeLogin(screen);
+
+    screen.addListener(closeLogin);
+
+    form.classList.toggle("show__login");
+    event.preventDefault();
+  });
+
+const closeLogin = document
+  .querySelector(".login__close")
+  .addEventListener("click", () => {
+    const form = document.querySelector(".login");
+
+    form.classList.remove("show__login");
+    event.preventDefault();
+  });
